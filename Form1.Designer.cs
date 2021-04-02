0c393b7adfe7f4a7696e02053ab42d27d02ebf10
Avdeev Denis <Avdeev02850285@yandex.ru>
2021-04-02 07:28:30 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Cross_inf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(268, 63);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(254, 46);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "Частотный анализ текста на триплеты";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(312, 145);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(162, 34);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Выберете  текстовой файл";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(312, 196);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(162, 34);
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "Выполнить анализ";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(211, 285);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(47, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Слова";
+            this.metroLabel1.Click += new System.EventHandler(this.metroLabel1_Click_2);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(210, 253);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(48, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "Время";
+            this.metroLabel2.Click += new System.EventHandler(this.metroLabel2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 520);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTile1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections;
+using System.IO;
+using System.Threading;
+
+
+
+namespace Cross_inf
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            Test1.GlobalVar = openFileDialog1.FileName;
+            // читаем файл в строку
+            Test1.GlobalVar = System.IO.File.ReadAllText(Test1.GlobalVar);
+
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            //Создаем первый поток
+            Thread thread1 = new Thread(watch);
+            thread1.Start();
+            metroLabel1.Text = "";
+            //Создаем второй поток
+            Thread thread2 = new Thread(osn);
+            thread2.Start();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+
+            var groups = SwimParts(Test1.GlobalVar, 3)
+                .GroupBy(str1 => str1);
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                Console.WriteLine(string.Join
+               (
+                   Environment.NewLine,
+                   groups.OrderByDescending(gr => gr.Count()).Take(10).Select(gr => $"\"{gr.Key}\" встретилось {gr.Count()} раз")
+               ));
+                string allConsoleOutput = stringWriter.ToString();
+                metroLabel1.Text += allConsoleOutput;
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Test2.GlobalVar = String.Format("{0:00} часов, {1:00} минут, {2:00} секунд, {3:000} миллисекунд",
+                        ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                Console.WriteLine("Время выполнения программы: " + Test2.GlobalVar);
+                string allConsoleOutput = stringWriter.ToString();
+                metroLabel2.Text = allConsoleOutput;
+            }
+
+        }
+        public static IEnumerable<string> SwimParts(string source, int length)
+        {
+            for (int i = length; i <= source.Length; i++)
+                yield return source.Substring(i - length, length);
+        }
+        static void watch()
+        {
+            //пока нет идей, что сюда вставить
+        }
+        public void osn()
+        {
+
+            //пока нет идей, что сюда вставить
+
+        }
+        static class Test1
+        {
+            private static string _globalVar;
+
+            public static string GlobalVar
+            {
+                get { return _globalVar; }
+                set { _globalVar = value; }
+            }
+        }
+
+        static class Test2
+        {
+            private static string _globalVar;
+
+            public static string GlobalVar
+            {
+                get { return _globalVar; }
+                set { _globalVar = value; }
+            }
+        }
+
+        static class Test3
+        {
+            private static string _globalVar;
+
+            public static string GlobalVar
+            {
+                get { return _globalVar; }
+                set { _globalVar = value; }
+            }
+        }
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
